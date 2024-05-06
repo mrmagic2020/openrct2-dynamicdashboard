5f576f2f9e2757109a42809d987b7497401d9549
Arnold Zhou <magicarnold2022@gmail.com>
2024-05-06 15:44:51 +1000

    chore: update targetApiVersion to 87

--- src/plugin.ts
+++ src/plugin.ts
@@ -14,6 +14,6 @@ registerPlugin({
 	 * version. Version 70 equals the v0.4.4 release.
 	 * @see https://github.com/OpenRCT2/OpenRCT2/blob/v0.4.4/src/openrct2/scripting/ScriptEngine.h#L50
 	 */
-	targetApiVersion: 77,
+	targetApiVersion: 87,
 	main: startup,
 });
