5a383ecdffb202052f41a24acbb7195b986a68c0
Arnold Zhou <magicarnold2022@gmail.com>
2024-05-06 19:44:25 +1000

    fix: language option not syncing with game config

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -4,10 +4,14 @@ import { en_US } from "./en-US"
 import { zh_CN } from "./zh-CN";
 import { WritableStore, store } from "openrct2-flexui";
 
+interface LanguageIndex {
+  [key: string]: Language;
+};
+
 /**
  * Index of all languages. **Do not alter order.**
  */
-const index = {
+const index: LanguageIndex = {
   en_US: en_US,
   zh_CN: zh_CN
 };
@@ -19,10 +23,7 @@ const index = {
  * 
  * @see index
  */
-const idList : [
-  "en_US",
-  "zh_CN"
-] = [
+const idList: string[] = [
   "en_US",
   "zh_CN"
 ];
@@ -42,12 +43,39 @@ const languageList = [
 /**
  * Store for language selection.
  */
-const languageStore : WritableStore<Language> = store<Language>(index[baseData.global.language.get()]);
+const languageStore: WritableStore<Language> = store<Language>(index[baseData.global.language.get()]);
+
+const defaultLanguage: string = "en_US";
 
 /**
  * Export extracted Language type for easy use. 
  */
-let language : Language = languageStore.get();
+let language: Language = languageStore.get();
+
+/**
+ * Get user's preferred language. 
+ * The result is formatted as `xx_YY` where `xx` is the language code and `YY` is the country code.
+ * 
+ * @returns {string} User's preferred language. 
+ */
+function getUserLanguage(): string {
+  let lang: string = context.configuration.get("general.language") || defaultLanguage;
+  lang = lang.replace("-", "_"); // replace hyphen with underscore
+  console.log("User set lang: " + lang);
+  return lang;
+}
+
+/**
+ * Get language index. 
+ * 
+ * @param lang Language id. 
+ * @returns {number} Language index. 
+ */
+function getLanguageIndex(lang: string): number {
+  let idx: number = idList.indexOf(lang);
+  if (idx == -1) idx = getLanguageIndex(defaultLanguage);
+  return idx;
+}
 
 /**
  * Replace `<insertable segments>` in a translation text with specified value. 
@@ -55,7 +83,7 @@ let language : Language = languageStore.get();
  * @param items Values to be inserted in order. 
  * @returns {string} Finalized text. 
  */
-function tr(str: string, ...items: any[]) : string {
+function tr(str: string, ...items: any[]): string {
   for (let i = 0; i < str.length; i++) {
     if (items.length === 0) break;
     let start, end;
@@ -79,25 +107,41 @@ function tr(str: string, ...items: any[]) : string {
  * 
  * @returns {void}
  */
-function initLang() : void {
+function initLang(): void {
   /**
-   * Update `language Store`
+   * Update `languageStore` when language is changed.
    * 
    * @see languageStore
    */
   baseData.global.language.subscribe((_value) => {
     languageStore.set(index[baseData.global.language.get()]);
+    // console.log("Language changed to " + baseData.global.language.get());
   });
 
   // update language
-  baseData.global.language_index.subscribe((value : number) => {
+  baseData.global.language_index.subscribe((value: number) => {
     baseData.global.language.set(idList[value]);
   });
 
   // update exported language object
-  languageStore.subscribe((_value : Language) => {
+  languageStore.subscribe((_value: Language) => {
     language = languageStore.get();
+    // console.log("Language updated.");
   });
+
+  /**
+   * Set language to user's preference or default language.
+   */
+  baseData.global.language_index.set(getLanguageIndex(getUserLanguage()));
+  console.log("User set lang: " + context.configuration.get("general.language"));
+  console.log("Language index set to " + baseData.global.language_index.get());
+
+  /**
+   * Update language every `update_ratio` seconds.
+   */
+  context.setInterval(() => {
+    baseData.global.language_index.set(getLanguageIndex(getUserLanguage()));
+  }, baseData.global.update_ratio.get() * 1000);
 }
 
 export { language, languageList, initLang, tr };
