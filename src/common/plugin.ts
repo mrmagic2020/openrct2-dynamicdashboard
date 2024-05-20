1bd1b80241d440f2d3cc9ff9e25919612e18c239
Arnold Zhou <magicarnold2022@gmail.com>
2024-05-20 21:03:45 +1000

    refactor: move plugin metadata to new file

---
+++ src/common/plugin.ts
@@ -0,0 +1,10 @@
+namespace DynamicDashboard {
+  export const name = "dynamicdashboard"
+  export const version = "1.0.0-pre.5"
+  export const authors = ["mrmagic2020"]
+  export const type = "intransient"
+  export const licence = "MIT"
+  export const targetApiVersion = 87
+}
+
+export default DynamicDashboard
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 /// <reference path="../lib/openrct2.d.ts" />
 
+import DynamicDashboard from "./common/plugin"
 import { startup } from "./startup"
 
 registerPlugin({
-  name: "dynamicdashboard",
-  version: "1.0.0-pre.5",
-  authors: ["mrmagic2020"],
-  type: "intransient",
-  licence: "MIT",
+  name: DynamicDashboard.name,
+  version: DynamicDashboard.version,
+  authors: DynamicDashboard.authors,
+  type: DynamicDashboard.type,
+  licence: DynamicDashboard.licence,
   /**
    * This field determines which OpenRCT2 API version to use. It's best to always use the
    * latest release version, unless you want to use specific versions from a newer develop
    * version. Version 70 equals the v0.4.4 release.
    * @see https://github.com/OpenRCT2/OpenRCT2/blob/v0.4.4/src/openrct2/scripting/ScriptEngine.h#L50
    */
-  targetApiVersion: 87,
+  targetApiVersion: DynamicDashboard.targetApiVersion,
   main: startup
 })
