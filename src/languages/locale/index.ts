2f2cb707d690299d33733e81f5a9b32e83e190dd
Arnold Zhou <magicarnold2022@gmail.com>
2024-05-20 13:50:12 +1000

    refactor: improve type definition for locales

--- src/languages/locale/index.ts
+++ src/languages/locale/index.ts
@@ -3,9 +3,12 @@ import _en_US from "./en-US.json"
 import _zh_CN from "./zh-CN.json"
 
 type Locale = typeof _en_GB
+type PartialLocale<T> = {
+  [P in keyof T]?: T[P] extends object ? PartialLocale<T[P]> : T[P]
+}
 
 const en_GB: Locale = _en_GB
-const en_US: Partial<Locale> = _en_US
-const zh_CN: Partial<Locale> = _zh_CN
+const en_US: PartialLocale<Locale> = _en_US
+const zh_CN: PartialLocale<Locale> = _zh_CN
 
 export { type Locale, en_GB, en_US, zh_CN }
